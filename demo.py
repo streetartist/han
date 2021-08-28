fa981fb17a409f0efab94b62d640b6227e265203
streetartist <60282562+streetartist@users.noreply.github.com>
2021-08-28 20:18:49 +0800

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,19 @@
+'''
+This is a file which shows how Han works like.
+BY StreetArtist Workroom
+'''
+
+def hello():
+    '''
+    This is a function which can print "Hello"
+    '''
+    print("Hello")
+    
+class Hello:
+    '''
+    The hello class
+    '''
+    def __init__():
+        '''
+        The init function
+        '''
